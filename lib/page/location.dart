dfa271e373154655a720e0617920058089d7ba20
Sayed Ar Rafi <96303632+sayedulabrar@users.noreply.github.com>
2023-03-29 04:07:41 +0600

    location complete by toky

--- lib/page/location.dart
+++ lib/page/location.dart
@@ -3,121 +3,179 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(CustomMarkerInfoWindowScreen());
 
+
 class CustomMarkerInfoWindowScreen extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  State<CustomMarkerInfoWindowScreen> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<CustomMarkerInfoWindowScreen> {
-  late GoogleMapController myMapController;
-  String dropdownValue = 'Office';
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  final Set<Marker> _markers = {};
+  final Set<Polygon> _polygons = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+  List<LatLng> polyPoints = [];
+  late Location myLocation;
 
-  final Set<Marker> _markers = new Set();
-  static const LatLng _mainLocation = const LatLng(24.7196, 90.4267);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Agriculture Office and shops'),
-              backgroundColor: Colors.blue[900],
-              centerTitle: true,
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  children: [
-                    Container(
-                      width: 180,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-                      child: DropdownButtonFormField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            //<-- SEE HERE
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            //<-- SEE HERE
-                            borderSide:
-                                BorderSide(color: Colors.black, width: 2),
-                          ),
-                          filled: true,
-                          fillColor: Colors.white,
-                        ),
-                        dropdownColor: Colors.white,
-                        value: dropdownValue,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        items: <String>['Office', 'Shops']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(
-                              value,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 18),
-                      width: 180,
-                      height: 100,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 15),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white60,
-                          hintText: "Location",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: GoogleMap(
-                    initialCameraPosition: CameraPosition(
-                      target: _mainLocation,
-                      zoom: 10.0,
-                    ),
-                    markers: this.myMarker(),
-                    mapType: MapType.normal,
-                    onMapCreated: (controller) {
-                      setState(() {
-                        myMapController = controller;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            )));
+  static final CameraPosition _position1 = CameraPosition(
+      bearing: 192.833,
+      target: LatLng(19.2215, 73.1645),
+      zoom: 12.0,
+      tilt: 59.440);
+  Future<void> _goToPosition1() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_position1));
   }
 
-  Set<Marker> myMarker() {
-    setState(() {
-      _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_mainLocation.toString()),
-        position: _mainLocation,
-        infoWindow: InfoWindow(
-          title: 'Historical City',
-          snippet: '5 Star Rating',
-        ),
-        icon: BitmapDescriptor.defaultMarker,
-      ));
-    });
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
 
-    return _markers;
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  _onTapMarkerAdd(LatLng latLng) {
+    setState(() {
+      polyPoints.add(latLng);
+      _drawPolygon(polyPoints);
+      _markers.add(
+        Marker(
+          draggable: true,
+          markerId: MarkerId(latLng.toString()),
+          position: latLng,
+          infoWindow: InfoWindow(title: 'title', snippet: 'snippet'),
+          icon: BitmapDescriptor.defaultMarker,
+          onDragEnd: (LatLng latLng) {
+            print(latLng);
+            print(latLng.toString());
+          },
+        ),
+      );
+    });
+  }
+
+  _clearAllMarkers() {
+    setState(() {
+      _markers.clear();
+      _polygons.clear();
+      polyPoints.clear();
+    });
+  }
+
+  _drawPolygon(List<LatLng> listLatLng) {
+    setState(() {
+      _polygons.add(Polygon(
+          polygonId: PolygonId('123'),
+          points: listLatLng,
+          fillColor: Colors.transparent,
+          strokeColor: Colors.greenAccent));
+    });
+  }
+
+  _calculateArea() {
+    polyPoints.add(polyPoints[0]);
+    print(calculatePolygonArea(polyPoints));
+    Fluttertoast.showToast(
+        msg: calculatePolygonArea(polyPoints).toString() + " Acres",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
+  static double calculatePolygonArea(List coordinates) {
+    double area = 0;
+
+    if (coordinates.length > 2) {
+      for (var i = 0; i < coordinates.length - 1; i++) {
+        var p1 = coordinates[i];
+        var p2 = coordinates[i + 1];
+        area += convertToRadian(p2.longitude - p1.longitude) *
+            (2 +
+                Math.sin(convertToRadian(p1.latitude)) +
+                Math.sin(convertToRadian(p2.latitude)));
+      }
+
+      area = area * 6378137 * 6378137 / 2;
+    }
+
+    return area.abs() * 0.000247105; //sq meters to Acres
+  }
+
+  static double convertToRadian(double input) {
+    return input * Math.pi / 180;
+  }
+
+  Widget button(Function() function, IconData icon) {
+    return FloatingActionButton(
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(icon, size: 36.0),
+      heroTag: icon.toString(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition:
+                CameraPosition(target: LatLng(23.83772, 90.35771), zoom: 14.0),
+            mapType: _currentMapType,
+            markers: _markers,
+            polygons: _polygons,
+            onCameraMove: _onCameraMove,
+            myLocationEnabled: true,
+            onTap: (LatLng latLng) {
+              _onTapMarkerAdd(latLng);
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  button(_onMapTypeButtonPressed, Icons.map),
+                  SizedBox(height: 16.0),
+                  button(_goToPosition1, Icons.location_searching),
+                  SizedBox(height: 16.0),
+                  button(_clearAllMarkers, Icons.location_off),
+                  SizedBox(height: 16.0),
+                  button(_calculateArea, Icons.av_timer),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
